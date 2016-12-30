7e038cbb5d3e46052886fe87ea94a5162cc13f49
laixiaolai <laiyongzhao@gmail.com>
2016-12-30 22:54:15 +0800

    添加demo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,6 @@
     <h2>Essential Links</h2>
   </div>
 </template>
-
 <script>
 export default {
   name: 'header',
@@ -21,23 +20,19 @@ export default {
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
 }
-
 ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
 }
